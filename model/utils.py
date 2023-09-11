cdaf38d3d515c5b506f7176ed1b3303dbd5ddfde
fawnliu <fawnliu2333@gmail.com>
2023-09-11 19:27:53 +0800

    u

--- model/utils.py
+++ model/utils.py
@@ -1,12 +1,6 @@
 import numpy as np 
 import torch 
 import torch.nn as nn
-# import transformers
-from transformers import RobertaModel, BertModel, GPT2LMHeadModel
-import torch.nn.functional as F
-
-import pdb 
-import CLIP.clip as clip 
 
 def Upsample(x, size):
     """
