7d2aa18d087f1e4bebd4cf9d73cb774736990330
fawnliu <fawnliu2333@gmail.com>
2024-05-25 12:22:05 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -19,7 +19,7 @@ def get_transform(size=None):
         ])
     else:
         transform = transforms.Compose([
-            transforms.Resize((224, 224)),
+            transforms.Resize((320, 320)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
